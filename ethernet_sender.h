0c71e303de377bb18224997ca02f868b68f3c26a
DogtailRay <lr19922014@163.com>
2013-07-20 06:24:27 +0800

    Update ethernet_sender.h

--- ethernet_sender.h
+++ ethernet_sender.h
@@ -12,6 +12,7 @@
 #define MAC_3 60:de:44:75:14:49(eth3)
 #define MAC_4 10:47:80:01:69:C8(eth4)
 
+
 //Sender packet struct
 typedef struct pkt_sender
 {
@@ -24,7 +25,7 @@ libnet_ptag_t p_tag;
 }sender_p;
 
 //functions
-sender_p* pkt_sender_init(char* iface, unsigned char dst[MAC_LEN], unsigned char src[MAC_LEN]);
+sender_p* pkt_sender_init(char* iface);
 int pkt_sender_send(sender_p* sender, char* data);
 void pkt_sender_close(sender_p* sender);
 int pkt_sender_send_out(sender_p* sender, char* data);
